fcaceda52b978ba0648e131004fe7308f7ad0a70
Jayesh Sharma <jayeshsh1707@gmail.com>
2023-06-02 19:04:32 +0530

    Add files via upload

---
+++ shop/script.js
@@ -0,0 +1,22 @@
+const allFilter = document.querySelector("#all-btn");
+const mensFilter = document.querySelector("#mens-btn");
+const womensFilter = document.querySelector("#womens-btn");
+const jewelleryFilter = document.querySelector("#jewellery-btn");
+const electronicsFilter = document.querySelector("#electronics-btn");
+
+//different sections
+const mensSection = document.querySelector("#mens-section");
+const womensSection = document.querySelector("#womens-section");
+const jewellerySection = document.querySelector("#jewellery-section");
+const electronicsSection = document.querySelector("#electronics-section");
+const searchSection = document.querySelector("#searched-section");
+
+const search = document.querySelector("#searchBar");
+const rangeBar=document.querySelector("#range")
+
+const applyBtn=document.querySelector("#apply-btn")
+const lowPrice=document.querySelector("#low")
+const midPrice=document.querySelector("#mid")
+const highPrice=document.querySelector("#high")
+const vHighPrice=document.querySelector("#vHigh")
+
